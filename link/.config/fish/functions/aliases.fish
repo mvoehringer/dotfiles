3dc98e0a838d6d38bab51cbdc55759e07b53ecaf
Michael Vöhringer <github@voehringer.net>
2014-03-19 20:42:35 +0100

    Add aliases

---
+++ link/.config/fish/functions/aliases.fish
@@ -0,0 +1,60 @@
+alias ..="cd .."
+alias ...="cd ../.."
+alias -="cd -"
+
+alias la="ls -Gla"
+
+# List only directories
+alias lsd='ls -l | grep "^d"'
+
+alias ll='ls -ahlF'
+alias l='ls -CF'
+
+################################
+###  File ShortCut
+################################
+alias D="cd ~/Downloads"
+alias p="cd ~/project"
+alias g="git"
+
+################################
+###  Program ShortCut
+################################
+
+# git related shortcut
+alias undopush="git push -f origin HEAD^:master"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias ga="git add"
+alias gca="git commit -a -m"
+alias gcm="git commit -m"
+alias gbd="git branch -D"
+alias gst="git status -sb --ignore-submodules"
+alias gm="git merge --no-ff"
+alias gpt="git push --tags"
+alias gp="git push"
+alias grs="git reset --soft"
+alias grh="git reset --hard"
+alias gb="git branch"
+alias gcob="git checkout -b"
+alias gco="git checkout"
+alias gba="git branch -a"
+alias gcp="git cherry-pick"
+alias gl="git lg"
+alias gpom="git pull origin master"
+# turn on coloring on grep
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Quick check running state of a process
+alias psef="ps -ef | grep "
+alias top="top -o cpu"
+alias ps="command ps -SAcr -o 'uid,pid,ppid,time,rss,command'"
+
+# from http://news.ycombinator.com/item?id=4492682
+function tree1; tree --dirsfirst -ChFLQ 1 $argv; end
+function tree2; tree --dirsfirst -ChFLQ 2 $argv; end
+function tree3; tree --dirsfirst -ChFLQ 3 $argv; end
+function tree4; tree --dirsfirst -ChFLQ 4 $argv; end
+function tree5; tree --dirsfirst -ChFLQ 5 $argv; end
+function tree6; tree --dirsfirst -ChFLQ 6 $argv; end
